1d247df9ffdfcd9229e78a2298fb560ca5eabfb1
Tim Esler <tim.esler@gmail.com>
2020-05-22 14:20:52 -0700

    Increment patch version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools, os
 
 PACKAGE_NAME = 'xt-training'
-VERSION = '1.10.0'
+VERSION = '1.10.1'
 AUTHOR = 'Xtract AI'
 EMAIL = 'info@xtract.ai'
 DESCRIPTION = 'Utilities for training models in pytorch'
