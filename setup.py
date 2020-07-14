37260c37d915f1df037414c25b40ca6efe1a5971
Tim Esler <tim.esler@gmail.com>
2020-07-13 18:48:03 -0700

    Increment minor version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools, os
 
 PACKAGE_NAME = 'xt-training'
-VERSION = '1.11.1'
+VERSION = '1.12.0'
 AUTHOR = 'Xtract AI'
 EMAIL = 'info@xtract.ai'
 DESCRIPTION = 'Utilities for training models in pytorch'
